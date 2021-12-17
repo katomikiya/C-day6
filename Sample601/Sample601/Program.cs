497dd389817f99ff7f34affaf3574154cbd67871
katomikiya <kato-m@omoteya.co.jp>
2021-12-17 10:09:14 +0900

    クラスとインスタンス

---
+++ Sample601/Sample601/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per1, per2;
+            per1 = new Person();
+            per2 = new Person();
+
+            per1.name = "山田太郎";
+            per1.age = 19;
+            per2.SetAgeAndName("佐藤花子", 23);
+
+            per1.showAgeAndName();
+            per2.showAgeAndName();
+        }
+    }
+}
